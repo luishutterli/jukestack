5643f41747ab23cfe4a8fbf9bd04ef73d0d69add
Luis Hutterli <lshutterli@gmail.com>
2025-01-06 15:04:12 +0100

    setup frontend vite react-ts project

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+	return (
+		<header>
+			<h1 className="font-extrabold">
+				JukeStack
+			</h1>
+		</header>
+	);
+}
+
+export default Header;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  colors: {
+    'text': '#06131c',
+    'background': '#edf7fd',
+    'primary': '#2786ec',
+    'secondary': '#af8be5',
+    'accent': '#b469dd',
+   },
+}
